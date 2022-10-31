ff1dca6a7e682eb8023e675a3449da6abf9bcb84
RafiNaufalYassarRamadhan <114968568+RafiNaufalYassarRamadhan@users.noreply.github.com>
2022-11-01 00:56:57 +0800

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,22 @@
+package com.f55121038.fragmentlayoutex;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class tes extends Fragment {
+    View view;
+    public tes(){
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.tes,container, false);
+        return view;
+    }
+
+}
